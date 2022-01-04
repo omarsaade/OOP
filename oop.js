0a00e757837c348d06c626c695d6d10a3975b0b5
omar saade <omarsaadeh11@gmail.com>
2022-01-04 22:38:31 +0200

    oop

--- oop.js
+++ oop.js
@@ -1,1 +1,28 @@
-// hello
+// OOP course
+
+
+
+
+// video 1 & 2
+// window            object
+//window.object.proprety
+//window.object.meth
+// window.location   w btenkatab hik window["location"]
+//el window hye object jaweto malyen object
+
+
+// window.location.reload proprety
+
+//window.location.reload()  methods
+
+// window["location"]["href"]
+// window["location"].href
+// so fi kaza tari2a lal kitebe ya kbiro
+
+//window.location.reload("https://google.com")
+
+
+
+
+// video 3
+// 
